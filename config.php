c636bb7e2ce4870fc27d14663d20ff113c95b47b
argajienbi <armin.gandi@gmail.com>
2018-12-04 23:49:03 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$access_token =
+"408c35d1-7909-423a-93ad-e6fb2b435009";//armin
+//$access_token = 
+"5c460252-4f20-42e3-8c77-58bde6f2a2b0";//anna
+//$access_token =
+"74ecf613-523d-4fcd-855c-235319bc3c01";//mutia
+//$access_token =
+"38193e06-dab0-458c-a97b-46b820206c2a";//mustofa
+//$access_token = 
+"cd139cb0-f6b5-458d-b956-9c5d8a3eed8e";//smk
+//$access_token = 
+"b4e859e0-5952-47d6-8bc1-4cc14526fdc4";//ato
+//$access_token =
+"af19c2a3-1d21-40dc-8e98-8ccf370a19ae";//fb
+?>
